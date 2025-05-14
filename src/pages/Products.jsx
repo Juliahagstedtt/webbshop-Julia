b81a05b809f48385bf7c6f2458cf736d75322c70
Julia Hagstedt <juliahagstedt@hotmail.com>
2025-05-14 19:49:50 +0200

    fixat sök och sortera

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,18 +8,17 @@ import shopIcon from '../assets/shopIcon.svg';
 import { useStore } from 'zustand';
 
 function Products() {
-    // state variabel. Börjar som en tom lista, men kommer snart innehålla alla produkter från databasen.
-    // "setProducts" används för att uppdatera innehållet i Products
     const [products, setProducts] = useState([]);
     const [counter, setCounter] = useState({});
+    const [sortOption, setSortOption] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
 
     // useEffect körs automatiskt EN gång när sidan laddas
     // Här används den för att hämta produktdata från Firestore
     useEffect(() => {
-        // En asynkron funktion, väntar på att hämta data från internet.
+//Asynkron funktion, väntar på att hämta data från internet.
         async function fetchData() {
-            
-            // Firebase hämtar ALLA dokument från samlingen som heter "products". Alla produkter som har sparats i firestore
+// Firebase hämtar ALLA dokument från samlingen som heter "products". Alla produkter som har sparats i firestore
             const querySnapshot = await getDocs(collection(db, "products"));
 
             const productsArray = querySnapshot.docs.map(doc => ({ 
@@ -40,26 +39,58 @@ function Products() {
             ...prev,
             [product.id]: (prev[product.id] || 0 ) + 1
         }));
-    };
+};    
 
+    const filteredAndSortedProducts = [...products]
+        .filter(product => 
+        (product.name?.toLowerCase().includes(searchTerm.toLowerCase()) || 
+        product.category?.toLowerCase().includes(searchTerm.toLowerCase()))
+        )
+        .sort((a, b) => {
+            switch (sortOption) {
+                case "name-asc":
+                    return a.name.localeCompare(b.name);
+                case "name-desc":
+                    return b.name.localeCompare(a.name);
+                case "price-asc":
+                    return a.price - b.price;
+                case "price-desc":
+                    return b.price - a.price;
+                default:
+                    return 0;
+            }
+        });
+
+        
     return (
         <>
         <div className="products-section">
             {/* Sökfält för produkter, (To Do! Inte gjord än)*/}
-            <input type="serch" placeholder="sök" className='serch'></input>
+            <input 
+                type="search" 
+                placeholder="Sök efter produkt..." 
+                className="search"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
 
             {/* Dropdown för att sortera produkter (To do! Inte gjord än) */}
-            <select className="dropdown">
+            <select
+                className="dropdown"
+                value={sortOption}
+                onChange={(e) => setSortOption(e.target.value)}
+            >
                 <option value="">Sortera</option>
-                <option value="dockor">Dockor</option>
-                <option value="kläder">Kläder & Accessoarer</option>
-                <option value="tillbehör">Barbie Livsstil</option>
+                <option value="name-asc">Namn A-Ö </option>
+                <option value="name-desc">Namn Ö-A</option>
+                <option value="price-asc">Pris lågt till högt</option>
+                <option value="price-desc">Pris högt till lågt</option>
             </select>
 
             <h1>Välkommen till Produktsidan!</h1>
         <div className='products'>
             {/* Renderar varje produkt */}
-            {products.map((product) => (
+            {filteredAndSortedProducts.map((product) => (
                 <div key={product.id} className='products-container'>
                     <h3>{product.name}</h3>
                     <p>{product.price} kr</p>
@@ -67,12 +98,12 @@ function Products() {
                     <p>{product.description}</p>
                     
                     {/* Knapp för att lägga till i varukorg */}
-                    <button className='shop-icon'>
+                    <button className='shop-icon' onClick={() => handleAddToCart(product)}>
                         <img 
                             src={shopIcon} 
                             alt="shop-icon" 
                             className="shop-icon" 
-                            onClick={() => addToCart(product)} // To Do: addToCart inte gjord än
+                            
                         />
                     </button>
                     </div>
