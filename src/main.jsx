7c76df15d91a18b6f129c15d8d4cd91ff1947a37
Julia Hagstedt <juliahagstedt@hotmail.com>
2025-05-19 06:44:31 +0200

    vite2

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/webbshop-Julia">
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
